d928da26e790e7073847185ccb084ce5c71701df
JimmyJ <jimmyj.inbox@gmail.com>
2016-11-20 19:27:43 -0600

    barebones complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,6 +9,9 @@
 
 
 //global variables
+var correctAs = 0;
+var incorrectAs = 0;
+var skippedQs = 0;
 var questionsAnswers = {
 	"qAs": [
 		{
@@ -46,23 +49,31 @@ $(".start").on('click', function() {
 	//start timer for 90 seconds
 	$("#timeLeft").html("<h2>Time Remaining: 90 seconds</h2>");
 	var timer = {
-		time: 100, //starting time
+		time: 10, //starting time
 		start: function () {  //function to start count
-			setInterval(timer.count, 1000);
+
+				startCount = setInterval(timer.count, 1000);	
+
+		},
+		stop: function () {
+			clearInterval(startCount);
 		},
 		count: function () { //actual counting function
-			console.log("count running");
+			//console.log("count running");
 			timer.time--;
 			if (timer.time>-1) {
 				$("#timeLeft").html("<h2>Time Remaining: "+ timer.time + " seconds</h2>");
 
-			} else {
+			} else if (timer.time=-1) {
 				$("#timeLeft").html("<h2>Time Remaining: OUT OF TIME!!!</h2><BR>");
-				$("#timeLeft").append("<h3>Answered Correctly:</h3><BR>");
-				$("#timeLeft").append("<h3>Answered Incorrectly:</h3><BR>");
-				$("#timeLeft").append("<h3>Not Answered At All:</h3><BR>");
-				return;
-			};
+				checkAnswers();
+				$("#timeLeft").append("<h3>Answered Correctly: "+correctAs+"</h3><BR>");
+				$("#timeLeft").append("<h3>Answered Incorrectly: "+incorrectAs+"</h3><BR>");
+				$("#timeLeft").append("<h3>Not Answered At All: "+skippedQs+"</h3><BR>");
+				timer.stop ();
+				$(".main").empty();
+				// return;
+			} else {};
 		},
 	};
 
@@ -70,13 +81,41 @@ $(".start").on('click', function() {
 	function displayQs () {
 		for (i = 0; i < questionsAnswers.qAs.length; i++) {
 			console.log("for loop worked " + i);  //test to see if worked
+			$(".main").append("<div class='form-group'>");
 			$(".main").append("<BR>"+questionsAnswers.qAs[i].question+"<BR><BR>"); //add question
-			for (j=0; j < 4; j++) { //add answers as radio buttons
-				console.log("logging i "+i)
-				$(".main").append("<div class='radio'><label><input type='radio' name='optionsRadios' id='optionRadios"+[j+1]+" value='"+[j+1]+"'>"+				questionsAnswers.qAs[i].answers[j]+"</label></div>");  
-			}
+			$(".main").append("<div class='col-lg-10 answers'>");
+			displayAs ();
+			$(".main").append("</div>");
+			$(".main").append("</div>");
 		}
 	};
+
+	function displayAs () {
+		for (j=0; j < 4; j++) { //add answers as radio buttons
+		console.log("logging i "+i)
+		$(".main").append("<div class='radio'><label><input type='radio' name='optionsRadios"+[i]+"' id='optionRadios"+[j+1]+"' value='"+[j+1]+"'>"+				questionsAnswers.qAs[i].answers[j]+"</label></div>");  
+			};
+	};
+
+	function checkAnswers () {
+
+		var len = questionsAnswers.qAs.length;
+		for (k=0; k<len; k++) {
+			var ansIndex=$('input[name=optionsRadios'+k+']:checked').val();
+			console.log("Answers made: "+ansIndex); //test
+			var ansComp=questionsAnswers.qAs[k].correctChoice
+			console.log("Correct Choices: "+ansComp) //test
+			if (ansIndex==ansComp) {
+				correctAs++;
+			} else if (ansIndex!=ansComp && ansIndex!=null) {
+				incorrectAs++;
+			} else {
+				skippedQs++;
+			}
+		};
+
+	};
+
 
 	//start timer
 	timer.start ();
